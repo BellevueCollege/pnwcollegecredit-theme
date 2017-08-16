a22cd6dae2e3217e3c2f14ad735ab565b678e415
John Kariungi <john.kariungi@bellevuecollege.edu>
2017-08-16 13:23:42 -0700

    Style child theme

--- functions.php
+++ functions.php
@@ -34,3 +34,60 @@ add_filter( 'wp_nav_menu_items','techprepcc_add_search_to_wp_menu',10,2 );
 
 /* Enable shortcodes in widget text */
 add_filter('widget_text', 'do_shortcode');
+
+								/*** Wide site ***/
+// 3.10 - Tip07 - Add new image size for custom post/page headers and select default header image.
+add_image_size( 'custom-header-image', 1120, 9999 ); // Unlimited height, soft crop
+
+if ( ! function_exists( 'tinyframework_mod_content_width' ) ) :
+/* Adjust content width in certain contexts.
+ *
+ * Adjust content_width value for full-width and single image attachment templates, and when there are no active widgets in the sidebar.
+ *
+ * template_redirect fires a lot later than after_setup_theme so that will still change the variable as usual.
+ */
+function tinyframework_mod_content_width() {
+	if ( is_page_template( 'page-templates/full-width.php' ) || is_attachment() || ! is_active_sidebar( 'sidebar-1' ) ) {
+		$GLOBALS['content_width'] = 1120;
+	}
+}
+endif;
+add_action( 'template_redirect', 'tinyframework_mod_content_width' );
+
+/**
+* Move Header Image Css 
+*/
+
+function techprepcc_header() {
+	wp_enqueue_style(
+		'custom_style',
+		 get_stylesheet_directory_uri() . "/style.css",
+		 array(),
+		 '1.0.0',
+		 'all'
+	);
+	$image_directory = get_header_image();
+	$custom_css = "
+		#masthead {	
+			background-image: url($image_directory);
+			background-repeat: no-repeat;
+    		background-position: center;
+			width:100%;
+			background-size: cover;
+		}";
+
+	wp_add_inline_style('custom_style', $custom_css);
+}
+
+add_action('wp_enqueue_scripts', 'techprepcc_header');
+
+/**
+* Exclude Category
+*/
+
+function exclude_category( $query ) {
+	if( $query->is_home() && $query->is_main_query() ) {
+		$query->set( 'cat', '-6' );
+	}
+}
+add_action( 'pre_get_posts', 'exclude_category' );
